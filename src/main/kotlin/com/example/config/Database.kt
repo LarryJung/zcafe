821d81c89f9ca30d2726252541815604651e7f7a
chaeda <chaeda@chaedaui-MacBookPro.local>
2024-03-29 21:18:53 +0900

    cafe-02-end 셋팅

--- src/main/kotlin/com/example/config/Database.kt
+++ src/main/kotlin/com/example/config/Database.kt
@@ -1,10 +1,23 @@
 package com.example.config
 
+import com.example.domain.CafeMenuTable
+import com.example.domain.CafeOrderTable
+import com.example.domain.CafeUserTable
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import io.ktor.server.application.*
 import org.h2.tools.Server
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.addLogger
+import org.jetbrains.exposed.sql.transactions.transaction
+import javax.sql.DataSource
 
 fun Application.configureDatabase() {
     configureH2()
+    connectDatabase()
+    initData()
 }
 
 
@@ -18,5 +31,30 @@ fun Application.configureH2() {
     environment.monitor.subscribe(ApplicationStopped) { application ->
         h2Server.stop()
         application.environment.log.info("H2 server stopped. ${h2Server.url}")
+    }
+}
+
+
+private fun connectDatabase() {
+    val config =
+        HikariConfig().apply {
+            jdbcUrl = "jdbc:h2:mem:cafedb"
+            driverClassName = "org.h2.Driver"
+            validate()
+        }
+
+    val dataSource: DataSource = HikariDataSource(config)
+    Database.connect(dataSource)
+}
+
+private fun initData() {
+    transaction {
+        addLogger(StdOutSqlLogger)
+
+        SchemaUtils.create(
+            CafeMenuTable,
+            CafeUserTable,
+            CafeOrderTable
+        )
     }
 }
