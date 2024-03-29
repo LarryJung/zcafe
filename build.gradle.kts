f19f2d9f456bf61b746865cb8551f6619f2f5b7b
chaeda <chaeda@chaedaui-MacBookPro.local>
2024-03-29 22:23:30 +0900

    cafe-06-start 셋팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,8 +30,10 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+    implementation("io.ktor:ktor-server-double-receive-jvm")
+    implementation("io.ktor:ktor-server-call-logging-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
