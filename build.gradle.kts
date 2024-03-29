7521c903bb378e5edb1fe4d9cacb36226c36926b
chaeda <chaeda@chaedaui-MacBookPro.local>
2024-03-29 22:12:03 +0900

    cafe-05-start 셋팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ val logback_version: String by project
 val exposed_version: String by project
 val h2_version: String by project
 val hikaricp_version: String by project
+val koin_version: String by project
 
 plugins {
     kotlin("jvm") version "1.9.23"
@@ -37,6 +38,8 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
     implementation("com.h2database:h2:$h2_version")
     implementation("com.zaxxer:HikariCP:$hikaricp_version")
+    implementation("io.insert-koin:koin-ktor:$koin_version")
+    implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
