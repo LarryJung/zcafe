c1e4d8ff91af15892e3ec71e551094e7d28ba15d
chaeda <chaeda@chaedaui-MacBookPro.local>
2024-03-29 21:00:53 +0900

    lecture 01 end 셋팅

--- src/main/kotlin/com/example/config/Routing.kt
+++ src/main/kotlin/com/example/config/Routing.kt
@@ -1,14 +1,53 @@
 package com.example.config
 
+import com.example.shared.CafeOrderStatus
+import com.example.shared.dto.OrderDto
+import com.example.shared.menuList
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.time.LocalDateTime
 
 
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        route("/api") {
+            get("/menus") {
+                val list = menuList
+                call.respond(list)
+            }
+            post("/orders") {
+                val request = call.receive<OrderDto.CreateRequest>()
+                val selectedMenu = menuList.first { it.id == request.menuId }
+                val order = OrderDto.DisplayResponse(
+                    orderCode = "ordercode1",
+                    menuName = selectedMenu.name,
+                    customerName = "홍길동",
+                    price = selectedMenu.price,
+                    status = CafeOrderStatus.READY,
+                    orderedAt = LocalDateTime.now(),
+                    id = 1
+                )
+                call.respond(order)
+            }
+            get("/orders/{orderCode}") {
+                val orderCode = call.parameters["orderCode"]!!
+                val order = OrderDto.DisplayResponse(
+                    orderCode = orderCode,
+                    menuName = "아이스라떼",
+                    customerName = "홍길동",
+                    price = 1000,
+                    status = CafeOrderStatus.READY,
+                    orderedAt = LocalDateTime.now(),
+                    id = 1
+                )
+                call.respond(order)
+            }
         }
     }
 }
--- src/main/kotlin/com/example/model/CafeMenu.kt
+++ src/main/kotlin/com/example/model/CafeMenu.kt
@@ -1,7 +1,9 @@
 package com.example.domain.model
 
 import com.example.shared.CafeMenuCategory
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class CafeMenu(
     val name: String,
     val price: Int,
